8d111fc846d3c36ba928dcbe4de9f3f92c45d35d
Meghanabetha17 <324103282015.meghana@gvpcew.ac.in>
2026-02-22 11:53:05 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+addition=a+b
+subtration=a-b
+mult=a*b
+di=a/b
+print("Addition:",addition)
+print("Subtration:",subtration)
+print("Multiplication:", mult)
+print('Division:',di)
+      
